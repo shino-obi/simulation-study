cd433ab7f1bc3131cf79e80116af2c0a1bc995ef
Lukas Higi <lukas.higi@pedeus.local>
2021-09-15 09:53:46 +0200

    scripts subfolder

---
+++ scripts/participant_data_transformation.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(lubridate)
+
+
+# add working directory path
+wd <- "P:/PhD/02 Simulationsstudie/simulation-study"
+
+setwd(wd)
+
+
+# read data
+raw_participant_info <- read.csv2("data/data_exp_56915-v22_questionnaire-d1do.csv", sep = ",", na.strings = c("", " ", "null"))
+
+# parse date
+raw_participant_info$Local.Date <- lubridate::dmy_hms(raw_participant_info$Local.Date, tz = "CET")
+
+# Description of gorilla data set columns (https://support.gorilla.sc/support/reference/faq/metrics#datacolumns)
+
+# Participant information
+# column: Participant.Private.ID = individual participant ID (for merging data sets - check live)
+# column: Question.Key = customary set question identifier
+# column: Response = participant response
+
+
+
+
+# select columns
+participant_columns <- raw_participant_info[,c("Participant.Private.ID",
+                                               "Question.Key",
+                                               "Response")]
+
+participant_columns_wide <- pivot_wider(data = participant_columns,id_cols = Participant.Private.ID, names_from = Question.Key,values_from = Response)
+participant_columns_wide$`BEGIN QUESTIONNAIRE` <- NULL
+participant_columns_wide$`END QUESTIONNAIRE` <- NULL
+
+# CREATE PARTICIPANT INFO DF
+
+
+
+# CREATE COVARIATES
