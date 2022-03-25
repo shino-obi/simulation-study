18f59316db66d8b4fef86af2c6e90d60d36094ec
Lukas Higi <lukas.higi@pedeus.local>
2022-03-25 17:09:48 +0100

    ranges splitted

--- scripts/evaluation_case_examples.R
+++ scripts/evaluation_case_examples.R
@@ -1,15 +1,41 @@
+library(tidyverse)
+
 # load transformed data
 load(file = "data/data_main.rda")
 
 
-# create column indicating calculation error = 1
-####################################### CHECK COLUMN TYPE FIRST #######################################
-data_main$is_error <- NA
-data_main$is_error <- if_else(condition = 
-                                data_main$response == data_main$true_result,
-                               true = paste0(0),
-                               false = paste0(1)
-)
+# split main dataset into df with dose ranges and with single dosages
+data_range <-
+  data_main %>% filter(str_detect(response, pattern = "-") == TRUE)
+
+data_single <-
+  data_main %>% filter(str_detect(response, pattern = "-") == FALSE)
+
+
+# data_range: create 2 new columns for lower and upper dose (for response and true result)
+# response
+split_response <-
+  str_split(string = data_range$response,
+            pattern = "-",
+            simplify = TRUE)
+
+split_response <- as.data.frame(split_response)
+colnames(split_response) <- c("response_lower", "response_upper")
+
+# true result
+split_true_result <-
+  str_split(string = data_range$true_result,
+            pattern = "-",
+            simplify = TRUE)
+
+split_true_result <- as.data.frame(split_true_result)
+colnames(split_true_result) <- c("true_lower", "ture_upper")
+
+# combine again
+data_range <- cbind(data_range, split_response, split_true_result)
+
+#
+
 
 
 ####################################### FOR PILOT #######################################
